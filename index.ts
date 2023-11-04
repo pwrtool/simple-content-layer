e6b2384b15b8af4a5892dc92f7f6caf4ebbcc08c
firesquid <jdeiss06@gmail.com>
2023-11-04 18:07:51 -0500

    add input getting

--- index.ts
+++ index.ts
@@ -3,8 +3,27 @@ import powertool from "@pwrtool/kit";
 powertool([
   {
     name: "default",
-    function: async (IO) => {
-      IO.out("Hello world!");
+    function: async (IO, CliArgs, Config) => {
+      const argsKeys = ["input", "output"];
+      const argValues = [];
+
+      for (const key of argsKeys) {
+        if (CliArgs.exists(key)) {
+          argValues.push(CliArgs.get(key));
+          continue;
+        }
+        if (Config.exists(key)) {
+          argValues.push(Config.get(key));
+          continue;
+        }
+
+        IO.error(`${key} could not be found in cli args or config`);
+        break;
+      }
+
+      const [input, output] = argValues;
+      IO.out(`input: ${input}`);
+      IO.out(`output: ${output}`);
     },
   },
 ]);
